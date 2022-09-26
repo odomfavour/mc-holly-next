2470ebfd8cf2aff3faa60ac968aa1a791d74471f
Chilo Nwokolo <chilo.nwokolo@gmail.com>
2022-09-26 15:48:15 +0100

    format accommodation

--- components/Accomodation.tsx
+++ components/Accomodation.tsx
@@ -1,72 +1,74 @@
-import React from 'react';
-import { FaLink, FaMapMarkerAlt } from 'react-icons/fa';
-import Image from 'next/image';
-import { hotels } from '../components/appData';
-import Link from 'next/link';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React from "react"
+import { FaLink, FaMapMarkerAlt } from "react-icons/fa"
+import Image from "next/image"
+import { hotels } from "../components/appData"
+import Link from "next/link"
+import { Swiper, SwiperSlide } from "swiper/react"
 // Import Swiper React components
 
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import "swiper/css"
+import "swiper/css/pagination"
+import "swiper/css/navigation"
 
 // import required modules
-import { Pagination, Navigation } from 'swiper';
+import { Pagination, Navigation } from "swiper"
 const Accomodation = () => {
-	return (
-		<section className="bg-[#E6D1DC] min-h-screen pb-20" id="accomodation">
-			<div className="w-11/12 mx-auto">
-				<div className="pt-20">
-					<h3 className="text-center mb-10 font-semibold  md:text-5xl text-2xl">
-						Accomodation
-					</h3>
-				</div>
+  return (
+    <section className="bg-[#E6D1DC] min-h-screen pb-20" id="accomodation">
+      <div className="w-11/12 mx-auto">
+        <div className="pt-20">
+          <h3 className="text-center mb-10 font-semibold  md:text-5xl text-2xl">
+            Accomodation
+          </h3>
+        </div>
 
-				<div>
-					<h3 className="text-[#821072] font-medium md:text-[32px] text-[20px] md:mb-4 mb-2">
-						Coming from outside Uyo?
-					</h3>
-					<p className="text-base font-normal mb-4">
-						Here are some hotels you can lodge for affordable prices
-					</p>
-					<div className="md:grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8 hidden">
-						{hotels.map((hotel) => {
-							const { id, name, link, address, image, price } = hotel;
-							return (
-								<div className="bg-white rounded-md" key={id}>
-									<Image
-										src={`${image.src}`}
-										width={600}
-										height={200}
-										objectFit="cover"
-										priority
-										className="rounded-tr-md rounded-tl-md"
-									/>
-									<div className="p-6">
-										<h4 className="font-medium text-[18px]">{name}</h4>
-										<p className="text-[14px] my-1">{address}</p>
-										<p className="mb-2">
-											<span className="text-[#821072]">₦{price}</span> avg/night
-										</p>
+        <div>
+          <h3 className="text-[#821072] font-medium md:text-[32px] text-[20px] md:mb-4 mb-2">
+            Coming from outside Uyo?
+          </h3>
+          <p className="text-base font-normal mb-4">
+            Here are some hotels you can lodge for affordable prices
+          </p>
+          <div className="md:grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8 hidden">
+            {hotels.map((hotel) => {
+              const { id, name, link, address, image, price } = hotel
+              return (
+                <div className="bg-white rounded-md" key={id}>
+                  <Image
+                    src={`${image.src}`}
+                    width={600}
+                    height={200}
+                    objectFit="cover"
+                    priority
+                    className="rounded-tr-md rounded-tl-md"
+                  />
+                  <div className="p-6">
+                    <h4 className="font-medium text-[18px]">{name}</h4>
+                    <p className="text-[14px] my-1">{address}</p>
+                    <p className="mb-2">
+                      <span className="text-[#821072]">₦{price}</span> avg/night
+                    </p>
 
-										<div className="flex justify-between items-center">
-											<a href={link} target="_blank">
-												<p className="rounded-md flex items-center bg-[#2252CC] py-1 px-4  text-white cursor-pointer">
-													Book <FaLink className="ml-2" />
-												</p>
-											</a>
-											<div className="flex items-center">
-												<FaMapMarkerAlt className="text-[#2252CC]" />
-												<Link href="#">
-													<p className="text-[#2252CC] text-[14px]">Uyo, Akwa Ibom</p>
-												</Link>
-											</div>
-										</div>
-									</div>
-								</div>
-							);
-						})}
-						{/* <div className="bg-white rounded-md">
+                    <div className="flex justify-between items-center">
+                      <a href={link} target="_blank">
+                        <p className="rounded-md flex items-center bg-[#2252CC] py-1 px-4  text-white cursor-pointer">
+                          Book <FaLink className="ml-2" />
+                        </p>
+                      </a>
+                      <div className="flex items-center">
+                        <FaMapMarkerAlt className="text-[#2252CC]" />
+                        <Link href="#">
+                          <p className="text-[#2252CC] text-[14px]">
+                            Uyo, Akwa Ibom
+                          </p>
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
+            {/* <div className="bg-white rounded-md">
               <Image
                 src="/monty.png"
                 width={600}
@@ -87,83 +89,84 @@ const Accomodation = () => {
                 </button>
               </div>
             </div> */}
-					</div>
+          </div>
 
-					<div className="md:hidden">
-						<Swiper
-							spaceBetween={50}
-							slidesPerView={8}
-							navigation
-							autoplay
-							modules={[Pagination, Navigation]}
-							breakpoints={{
-								320: {
-									slidesPerView: 1,
-									spaceBetween: 50,
-								},
-								375: {
-									slidesPerView: 1,
-									spaceBetween: 10,
-								},
-								// when window width is <= 999px
-								768: {
-									slidesPerView: 1,
-									spaceBetween: 10,
-								},
-								999: {
-									slidesPerView: 1,
-									spaceBetween: 10,
-								},
-							}}
-							onSlideChange={() => console.log('slide change')}
-							onSwiper={(swiper) => console.log(swiper)}
-						>
-							{hotels.map((hotel) => {
-								const { id, name, link, address, image, price } = hotel;
-								return (
-									<SwiperSlide key={id}>
-										<div className="bg-white rounded-md">
-											<Image
-												src={`${image.src}`}
-												width={600}
-												height={200}
-												objectFit="cover"
-												priority
-												className="rounded-tr-md rounded-tl-md"
-											/>
-											<div className="p-6">
-												<h4 className="font-medium text-[18px]">{name}</h4>
-												<p className="text-[14px] my-1">{address}</p>
-												<p className="mb-2">
-													<span className="text-[#821072]">₦{price}</span> avg/night
-												</p>
+          <div className="md:hidden">
+            <Swiper
+              spaceBetween={50}
+              slidesPerView={8}
+              navigation
+              autoplay
+              modules={[Pagination, Navigation]}
+              breakpoints={{
+                320: {
+                  slidesPerView: 1,
+                  spaceBetween: 50,
+                },
+                375: {
+                  slidesPerView: 1,
+                  spaceBetween: 10,
+                },
+                // when window width is <= 999px
+                768: {
+                  slidesPerView: 1,
+                  spaceBetween: 10,
+                },
+                999: {
+                  slidesPerView: 1,
+                  spaceBetween: 10,
+                },
+              }}
+              onSlideChange={() => console.log("slide change")}
+              onSwiper={(swiper) => console.log(swiper)}
+            >
+              {hotels.map((hotel) => {
+                const { id, name, link, address, image, price } = hotel
+                return (
+                  <SwiperSlide key={id}>
+                    <div className="bg-white rounded-md">
+                      <Image
+                        src={`${image.src}`}
+                        width={600}
+                        height={200}
+                        objectFit="cover"
+                        priority
+                        className="rounded-tr-md rounded-tl-md"
+                      />
+                      <div className="p-6">
+                        <h4 className="font-medium text-[18px]">{name}</h4>
+                        <p className="text-[14px] my-1">{address}</p>
+                        <p className="mb-2">
+                          <span className="text-[#821072]">₦{price}</span>{" "}
+                          avg/night
+                        </p>
 
-												<div className="flex justify-between items-center">
-													<Link href={link}>
-														<p className="rounded-md flex items-center bg-[#2252CC] py-1 px-4  text-white cursor-pointer">
-															Book <FaLink className="ml-2" />
-														</p>
-													</Link>
-													<div className="flex items-center">
-														<FaMapMarkerAlt className="text-[#2252CC]" />
-														<Link href="#">
-															<p className="underline text-[#2252CC] text-[14px] cursor-pointer">
-																kjdfkjdfjkdf
-															</p>
-														</Link>
-													</div>
-												</div>
-											</div>
-										</div>
-									</SwiperSlide>
-								);
-							})}
-						</Swiper>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
-};
+                        <div className="flex justify-between items-center">
+                          <Link href={link}>
+                            <p className="rounded-md flex items-center bg-[#2252CC] py-1 px-4  text-white cursor-pointer">
+                              Book <FaLink className="ml-2" />
+                            </p>
+                          </Link>
+                          <div className="flex items-center">
+                            <FaMapMarkerAlt className="text-[#2252CC]" />
+                            <Link href="#">
+                              <p className="underline text-[#2252CC] text-[14px] cursor-pointer">
+                                kjdfkjdfjkdf
+                              </p>
+                            </Link>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                )
+              })}
+            </Swiper>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
 
-export default Accomodation;
+export default Accomodation
